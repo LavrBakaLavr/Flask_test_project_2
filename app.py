1970dfbcd70ce5e13192131cf360273a92a8570d
LavrBakaLavr <79411954+LavrBakaLavr@users.noreply.github.com>
2022-02-11 14:39:08 +0800

    Change route => class

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity
 from config import Config
+from flask.views import MethodView
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -25,36 +26,44 @@ from models import *
 
 Base.metadata.create_all(bind=engine)
 
-@app.route('/info', methods=['GET'])
-@jwt_required()
-def get_info():
-    user_id = get_jwt_identity()
-    wallet = User.query.filter(User.id == user_id)
+class InfoAPI(MethodView):
+    decorators = [jwt_required()]
+    def get(self):
+        user_id = get_jwt_identity()
+        wallet = User.query.filter(User.id == user_id)
 
-    if wallet[0].wallet <= 0 :
-        return jsonify({'error': 'Your wallet is empty'})
-        
-    else :
-        wallet[0].wallet = wallet[0].wallet - 1
+        if wallet[0].wallet <= 0 :
+            return jsonify({'error': 'Your wallet is empty'})
+            
+        else :
+            wallet[0].wallet = wallet[0].wallet - 1
+            session.commit()
+
+            return jsonify({'wallet': wallet[0].wallet})
+
+info_view = InfoAPI.as_view('info_api')
+
+app.add_url_rule('/info', view_func=info_view, methods=['GET'])
+
+class UserAPI(MethodView):
+    def get(self):
+        params = request.json
+        user = User.authenticate(**params)
+        token = user.get_token()
+        return {'access_token': token}
+
+    def post(self):
+        params = request.json
+        user = User(**params)
+        session.add(user)
         session.commit()
+        token = user.get_token()
+        return {'access_token': token}
 
-        return jsonify({'wallet': wallet[0].wallet})
+user_view = UserAPI.as_view('user_api')
 
-@app.route('/register', methods=['POST'])
-def register():
-    params = request.json
-    user = User(**params)
-    session.add(user)
-    session.commit()
-    token = user.get_token()
-    return {'access_token': token}
-
-@app.route('/auth', methods=['POST'])
-def auth():
-    params = request.json
-    user = User.authenticate(**params)
-    token = user.get_token()
-    return {'access_token': token}
+app.add_url_rule('/auth', view_func=user_view, methods=['GET'])
+app.add_url_rule('/register', view_func=user_view, methods=['POST'])
     
 @app.teardown_appcontext
 def shutdown_session(exception=None):
